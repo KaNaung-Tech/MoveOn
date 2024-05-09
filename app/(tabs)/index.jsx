93823f180fd40f8ac6c3a2416ed51d70b3079ffb
nyeinminhtet <nyeinmg904@gmail.com>
2024-05-09 22:01:07 +0630

    remove:detailscreen

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,20 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
-import DetailScreenContainer from "@/components/DetailScreenContainer";
 
 const Home = () => {
   return (
-    <DetailScreenContainer title="Home">
-      <View style={{ marginTop: 100 }}>
-        <Text style={{ fontSize: 20, fontWeight: "600", color: "white" }}>
-          Home
-        </Text>
-        <Link href="/(user)"> go to profile</Link>
-        <Link href="/journaling/[mode]"> go to journaling</Link>
-        <Link href="/(user)/setting"> go to setting</Link>
-      </View>
-    </DetailScreenContainer>
+    <View style={{ marginTop: 100 }}>
+      <Text style={{ fontSize: 20, fontWeight: "600", color: "white" }}>
+        Home
+      </Text>
+      <Link href="/(user)"> go to profile</Link>
+      <Link href="/journaling/[mode]"> go to journaling</Link>
+      <Link href="/(user)/setting"> go to setting</Link>
+    </View>
   );
 };
 
