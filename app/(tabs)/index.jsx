26bff284923de19ea4d1f5a886dbcb366dafcb8a
nyeinminhtet <nyeinmg904@gmail.com>
2024-05-09 22:02:46 +0630

    add:detailscreen-container-ui

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,17 +1,20 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import DetailScreenContainer from "@/components/DetailScreenContainer";
 
 const Home = () => {
   return (
-    <View style={{ marginTop: 100 }}>
-      <Text style={{ fontSize: 20, fontWeight: "600", color: "white" }}>
-        Home
-      </Text>
-      <Link href="/(user)"> go to profile</Link>
-      <Link href="/journaling/[mode]"> go to journaling</Link>
-      <Link href="/(user)/setting"> go to setting</Link>
-    </View>
+    <DetailScreenContainer title="Home">
+      <View style={{ marginTop: 100 }}>
+        <Text style={{ fontSize: 20, fontWeight: "600", color: "white" }}>
+          Home
+        </Text>
+        <Link href="/(user)"> go to profile</Link>
+        <Link href="/journaling/[mode]"> go to journaling</Link>
+        <Link href="/(user)/setting"> go to setting</Link>
+      </View>
+    </DetailScreenContainer>
   );
 };
 
