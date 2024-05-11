076332ffdedbaed830385d8942de33ac38df9250
Zar Hlyan Aung <zarhlyanaung.ai@gmail.com>
2024-05-11 19:42:31 +0630

    error: creating onboard, link doesn't go there from Home to onboard

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,18 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { Link } from "expo-router";
-import DetailScreenContainer from "@/components/DetailScreenContainer";
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Link } from 'expo-router';
+import DetailScreenContainer from '@/components/DetailScreenContainer';
 
 const Home = () => {
   return (
     <DetailScreenContainer title="Home">
       <View style={{ marginTop: 100 }}>
-        <Text style={{ fontSize: 20, fontWeight: "600", color: "white" }}>
+        <Text style={{ fontSize: 20, fontWeight: '600', color: 'white' }}>
           Home
         </Text>
         <Link href="/(user)"> go to profile</Link>
         <Link href="/journaling/[mode]"> go to journaling</Link>
         <Link href="/(user)/setting"> go to setting</Link>
+        <Link href="/onboard"> go to onboard</Link>
       </View>
     </DetailScreenContainer>
   );
