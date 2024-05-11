11b6601e146fa6c09e0a697da3b80a124dd0638a
Aung Kaung Myat <115795473+aungkaungmyat9504@users.noreply.github.com>
2024-05-11 20:09:33 +0700

    Complete home screen and main layout

---
+++ constants/data.tsx
@@ -0,0 +1,22 @@
+import { GalleryIcon, QuestionIcon, ReadingIcon } from "@/assets/icons";
+
+export const ACTIVITIES_DATA = [
+  {
+    icon: <ReadingIcon />,
+    title: "Reading",
+    subTitle: "Positive Quotes",
+    route: "",
+  },
+  {
+    icon: <QuestionIcon />,
+    title: "Questions",
+    subTitle: "Self-Discovery",
+    route: "",
+  },
+  {
+    icon: <GalleryIcon />,
+    title: "Activities",
+    subTitle: "1 Week Activities",
+    route: "",
+  },
+];
